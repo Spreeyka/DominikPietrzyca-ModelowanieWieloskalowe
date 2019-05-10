c294e6879a0f653b3206b215aeadd590e186a109
Java_GCL05_lato_2018_Dominik_Pietrzyca <37092171+Spreeyka@users.noreply.github.com>
2019-05-10 18:13:24 +0200

    Automaty komórkowe

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(88, 160);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(50, 10, 10, 10);
+            this.panel1.Size = new System.Drawing.Size(901, 449);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(726, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(328, 52);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(94, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Wysokość";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Szerokość";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Metoda";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(877, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 39);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "zamknięte",
+            "periodyczne"});
+            this.comboBox1.Location = new System.Drawing.Point(447, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(569, 50);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(78, 22);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(444, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Warunek";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(566, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Liczba iteracji";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 667);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    
+    public partial class Form1 : Form
+    {
+        int width;
+        int nwidth;
+        int height;
+        int nheight;
+        int method;
+        int counter;
+        static int iteration = 0;
+        bool[,] logicTable;
+        int licznik = 1;
+        string condition;
+        Pen p = new Pen(Color.Black, 1);
+        Brush w = new SolidBrush(Color.White);
+        Brush brush = new SolidBrush(Color.Black);
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+            
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            condition = comboBox1.Text;
+            if (textBox1.Text == "") width = 16;  else width = Convert.ToInt32(textBox1.Text);
+            if (textBox2.Text == "") height = 31; else height = Convert.ToInt32(textBox2.Text);
+            if (textBox3.Text == "") method = 250; else method = Convert.ToInt32(textBox3.Text);
+            if (textBox4.Text == "\r\n") counter = 1;  else counter = Convert.ToInt32(textBox4.Text);
+            if (condition == "") condition = "periodyczne";
+            
+
+
+            string binaryString = Convert.ToString(method, 2).PadLeft(8,'0');
+            
+
+            if (licznik==1 || nwidth != width || nheight != height)
+            {
+                logicTable = new bool[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            logicTable[i, j] = new bool();
+                        }
+                    }
+                logicTable[0, height / 2] = true;
+                licznik = 0;
+            }
+            nwidth = width;
+            nheight = height;
+
+            for (int i = 0; i < counter; i++)
+            {
+                drawTable(logicTable, panel1);
+
+                if (iteration < width - 1)
+                {
+                    if (condition == "periodyczne")
+                    {
+                        setPeriodicNeightbours(binaryString, iteration);
+                    }
+                    else
+                    {
+                        selectNeightbours(binaryString, iteration);
+                    }
+                }
+                ++iteration;
+            }
+                
+            if (iteration == width-1)
+            {
+                button1.Visible = false;
+            }
+
+            
+        }
+
+        public void drawTable(bool[,] logicTable, Panel panel)
+        {
+            float x = 0;
+            float y = 0;
+            float squareLength;
+            Graphics l = panel.CreateGraphics();
+            if (width > height)
+            {
+                squareLength = panel.Height / width;
+                x = panel.Width / 4;
+            }
+            else
+            {
+
+                squareLength = panel.Height / height;
+                x = panel.Width / 4;
+
+            }
+                   
+            for (int i = 0; i < width; ++i)
+            {
+                for (int j = 0; j < height; ++j)
+                {
+                    if (logicTable[i, j] == false)
+                    {
+                        l.FillRectangle(w, x, y, squareLength, squareLength);
+                    }
+                    if (logicTable[i, j] == true)
+                    {
+                        l.FillRectangle(brush, x, y, squareLength, squareLength);
+                    }
+                    x += squareLength;
+                }
+                if (width > height)
+                {
+                    x = panel.Width / 4;
+                }
+                else
+                {
+                    x = panel.Width / 4;
+                }
+                
+                y += squareLength;
+            }
+        }
+        public void selectNeightbours(string method, int i) //i to numer iteracji/rzędu
+        {
+            for (int j = 0; j < height; ++j)
+                if (j-1!=-1 && j+1 !=height)
+                {
+                    {
+                        if (method.ElementAt(0) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == true && logicTable[i, j + 1] == true)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(1) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == true && logicTable[i, j + 1] == false)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(2) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == false && logicTable[i, j + 1] == true)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(3) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == false && logicTable[i, j + 1] == false)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(4) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == true && logicTable[i, j + 1] == true)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(5) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == true && logicTable[i, j + 1] == false)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(6) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == false && logicTable[i, j + 1] == true)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                        if (method.ElementAt(7) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == false && logicTable[i, j + 1] == false)
+                        {
+                            logicTable[i + 1, j] = true;
+                        }
+                    }
+                }
+            
+        }
+
+        //w tym przypadku lepsze byloby uzycie listy a nie typu wartosciowego
+
+        public void setPeriodicNeightbours (string method, int i)
+        {
+            for (int j = 0; j < height; ++j)
+
+            {
+                if (j - 1 != -1  && j + 1 != height)
+                {
+                    if (method.ElementAt(0) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == true && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(1) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == true && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(2) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == false && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(3) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == false && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(4) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == true && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(5) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == true && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(6) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == false && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(7) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == false && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                }
+
+                if (j - 1 == -1)
+                {
+                    if (method.ElementAt(0) == '1' && logicTable[i, height-1] == true && logicTable[i, j] == true && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(1) == '1' && logicTable[i, height - 1] == true && logicTable[i, j] == true && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(2) == '1' && logicTable[i, height - 1] == true && logicTable[i, j] == false && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(3) == '1' && logicTable[i, height - 1] == true && logicTable[i, j] == false && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(4) == '1' && logicTable[i, height - 1] == false && logicTable[i, j] == true && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(5) == '1' && logicTable[i, height - 1] == false && logicTable[i, j] == true && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(6) == '1' && logicTable[i, height - 1] == false && logicTable[i, j] == false && logicTable[i, j + 1] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(7) == '1' && logicTable[i, height - 1] == false && logicTable[i, j] == false && logicTable[i, j + 1] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                }
+                if (j + 1 == height)
+                {
+                    if (method.ElementAt(0) == '1' && logicTable[i, j-1] == true && logicTable[i, j] == true && logicTable[i, 0] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(1) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == true && logicTable[i, 0] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(2) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == false && logicTable[i, 0] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(3) == '1' && logicTable[i, j - 1] == true && logicTable[i, j] == false && logicTable[i, 0] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(4) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == true && logicTable[i, 0] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(5) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == true && logicTable[i, 0] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(6) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == false && logicTable[i, 0] == true)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                    if (method.ElementAt(7) == '1' && logicTable[i, j - 1] == false && logicTable[i, j] == false && logicTable[i, 0] == false)
+                    {
+                        logicTable[i + 1, j] = true;
+                    }
+                }
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {        
+                logicTable = new bool[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            logicTable[i, j] = new bool();
+                        }
+                    }
+                logicTable[0, height / 2] = true;
+                iteration = 0;
+                panel1.Refresh();
+                button1.Visible = true;
+        }      
+    }  
+}
