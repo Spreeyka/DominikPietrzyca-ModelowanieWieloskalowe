6d5ade2a89f8f2045df3e07bbd27685c24c139cd
Java_GCL05_lato_2018_Dominik_Pietrzyca <37092171+Spreeyka@users.noreply.github.com>
2019-06-23 04:54:27 +0200

    Add files via upload
    gra w życie

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(873, 612);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(949, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(949, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(949, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(949, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "niezmienne",
+            "glider",
+            "oscylator",
+            "losowe",
+            "ręczne"});
+            this.comboBox1.Location = new System.Drawing.Point(949, 212);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(946, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Szerokość";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(946, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Wysokość";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(946, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Wzór";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(949, 432);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 40);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 657);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,514 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int width;
+        int nwidth;
+        int height;
+        int nheight;
+        string pattern;
+        static bool[,] logicTable;
+        static bool[,] logicTable2;
+        static bool[,] logicTable3;
+        static bool[,] logicTable4;
+        int licznik = 1;
+        static int iteration = 0;
+        bool[] neighbours;
+        double startingX;
+        double startingY;
+        bool isWorking = true;
+        Pen p;
+        Brush brush;
+        Graphics l;
+        Thread thread1;
+        float x;
+        float y;
+        float squareLength;
+
+
+        public Form1()
+        { 
+            p = new Pen(Color.Black, 1);
+            brush = new SolidBrush(Color.Black);
+            
+            neighbours = new bool[8];          
+            InitializeComponent();
+            
+        }
+        //musze zrobic zeby nie resetowal sie stan za kazdym razem, bo nie moge teraz naklikac co chce zmienic. Edit reset button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isWorking = true;
+            thread1 = new Thread(new ThreadStart(start));
+            thread1.Start();
+        }
+        public void start()
+        {
+
+            //pictureBox1.Refresh();
+            if (textBox1.Text == "") width = 10; else width = Convert.ToInt32(textBox1.Text);
+            if (textBox2.Text == "") height = 10; else height = Convert.ToInt32(textBox2.Text);
+            if (comboBox1.Text == "") pattern = "oscylator"; else pattern = comboBox1.Text;
+
+            if (licznik == 1 || nwidth != width || nheight != height)
+            {
+
+                logicTable = new bool[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            logicTable[i, j] = new bool();
+                        }
+                    }
+                logicTable2 = new bool[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            logicTable2[i, j] = new bool();
+                        }
+                    }
+                logicTable3 = new bool[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            logicTable3[i, j] = new bool();
+                        }
+                    }
+                logicTable4 = new bool[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            logicTable4[i, j] = new bool();
+                        }
+                    }
+                setInitialPattern();
+                licznik = 0;
+            }
+            nwidth = width;
+            nheight = height;
+
+            while (isWorking)
+            {
+
+
+                Clear(logicTable4);
+                if (iteration % 2 == 0)
+                {
+                    
+                    drawTable(logicTable);
+                    setNeightbours(logicTable, logicTable2);
+
+                    for (int i = 0; i < width; ++i)
+                        for (int j = 0; j < height; j++)
+                        {
+                            {
+                                logicTable3[i, j] = logicTable[i, j];
+                            }
+                        }
+                    for (int i = 0; i < width; ++i)
+                        for (int j = 0; j < height; j++)
+                        {
+                            {
+                                logicTable[i, j] = false; 
+                            }
+                        }
+                    
+                }
+                if (iteration % 2 == 1)
+                {
+                    
+                    drawTable(logicTable2); 
+                    setNeightbours(logicTable2, logicTable); // setNeightbours(logicTable3, logicTable2) a pozniej to <--
+
+                    for (int i = 0; i < width; ++i)
+                        for (int j = 0; j < height; j++)
+                        {
+                            {
+                                logicTable3[i, j] = logicTable2[i, j];
+                            }
+                        }
+                    for (int i = 0; i < width; ++i)          // z tego 1231
+                        for (int j = 0; j < height; j++)     // dlaczego pozniej znika?
+                        {
+                            {
+                                logicTable2[i, j] = false;
+                            }
+                        }
+                    
+                }
+                
+                ++iteration;
+                
+                Thread.Sleep(200);
+                
+                
+
+
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            l = pictureBox1.CreateGraphics();
+            MouseEventArgs e2 = (MouseEventArgs)e;
+            int ff = (int)Math.Floor(((e2.X - startingX) / squareLength));
+            int gg = (int)Math.Floor(((e2.Y - startingY) / squareLength));
+
+            logicTable3[ff, gg] = true;
+            if (iteration % 2 == 1)
+            {
+                
+                setNeightbours(logicTable3, logicTable2);
+                //setNeightbours(logicTable3, logicTable);
+            }
+
+            if (iteration % 2 == 0)
+            {
+                
+                setNeightbours(logicTable3, logicTable);
+                //setNeightbours(logicTable3, logicTable);
+
+            }
+
+            l.FillRectangle(brush, (float)startingX + (ff * squareLength), gg * squareLength, squareLength, squareLength);
+
+            
+        }
+        public void drawTable(bool[,] logicTable)
+        {
+            x = 0;
+            y = 0;
+            startingY = y;
+            l = pictureBox1.CreateGraphics();
+            if (width > height)
+            {
+                squareLength = pictureBox1.Height / width;
+                x = pictureBox1.Width / 4;
+                startingX = x;
+            }
+            else
+            {
+
+                squareLength = pictureBox1.Height / height;
+                x = pictureBox1.Width / 4;
+                startingX = x;
+
+            }
+
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    if (logicTable[j, i] == false)
+                    {
+                        l.DrawRectangle(p, x, y, squareLength, squareLength);
+                    }
+                    if (logicTable[j, i] == true)
+                    {
+                        l.FillRectangle(brush, x, y, squareLength, squareLength);
+                    }
+                    x += squareLength;
+                }
+                if (width > height)
+                {
+                    x = pictureBox1.Width / 4;
+                }
+                else
+                {
+                    x = pictureBox1.Width / 4;
+                }
+
+                y += squareLength;
+            }
+           
+        }
+        public void Clear(bool[,] logicTable)
+        {
+            x = 0;
+            y = 0;
+            startingY = y;
+            l = pictureBox1.CreateGraphics();
+            if (width > height)
+            {
+                squareLength = pictureBox1.Height / width;
+                x = pictureBox1.Width / 4;
+                startingX = x;
+            }
+            else
+            {
+
+                squareLength = pictureBox1.Height / height;
+                x = pictureBox1.Width / 4;
+                startingX = x;
+
+            }
+
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    if (logicTable[j, i] == false)
+                    {
+                        l.FillRectangle(Brushes.White, x, y, squareLength, squareLength);
+                    }
+                    if (logicTable[j, i] == true)
+                    {
+                        l.FillRectangle(Brushes.White, x, y, squareLength, squareLength);
+                    }
+                    x += squareLength;
+                }
+                if (width > height)
+                {
+                    x = pictureBox1.Width / 4;
+                }
+                else
+                {
+                    x = pictureBox1.Width / 4;
+                }
+
+                y += squareLength;
+            }
+
+        }
+
+        public void setInitialPattern()
+        {
+            if(pattern == "niezmienne")
+            {
+                logicTable[width / 2 - 1, height / 2] = true;
+                logicTable[width / 2, height / 2 + 1] = true;
+                logicTable[width / 2 + 1, height / 2 + 1] = true;
+                logicTable[width / 2 + 2, height / 2] = true;
+                logicTable[width / 2, height / 2 - 1] = true;
+                logicTable[width / 2 + 1, height / 2 - 1] = true;
+            }
+
+            if (pattern == "glider")
+            {
+                logicTable[width / 2 - 1, height / 2] = true;
+                logicTable[width / 2, height / 2 - 1] = true;
+                logicTable[width / 2, height / 2] = true;
+                logicTable[width / 2 + 1, height / 2 - 1] = true;
+                logicTable[width / 2 + 1, height / 2 + 1] = true;
+                
+            }
+
+            if (pattern == "oscylator")
+            {
+                logicTable[width / 2 - 1, height / 2 ] = true;
+                logicTable[width / 2 - 0, height / 2 ] = true;
+                logicTable[width / 2 + 1, height / 2 ] = true;
+            }
+
+            if (pattern == "losowe")
+            {
+                Random rand = new Random();
+                
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; j++)
+                    {
+                        {
+                            if (rand.NextDouble() >= 0.7) logicTable[i, j] = true;                        
+                        }
+                    }
+            }
+            if (pattern == "ręczne")
+            {
+             
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            isWorking = false;
+            thread1.Join();
+            button1.Visible = true;
+
+            
+        }
+
+        public void setNeightbours(bool[,] logicTable, bool[,] logicTable2)
+        {
+
+            
+            
+            for (int k = 0; k < height; ++k)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    if( j - 1 == -1 && 0 < k && k < height - 1 )
+                    {
+                        neighbours[0] = logicTable[width-1, k - 1];
+                        neighbours[1] = logicTable[width-1, k];
+                        neighbours[2] = logicTable[width-1, k + 1];
+
+                        neighbours[3] = logicTable[j , k - 1];
+                        neighbours[4] = logicTable[j , k + 1];
+
+                        neighbours[5] = logicTable[j + 1, k - 1];
+                        neighbours[6] = logicTable[j + 1, k];
+                        neighbours[7] = logicTable[j + 1, k + 1];
+                    }
+                    if (k - 1 == -1 && 0 < j && j < width - 1)
+                    {
+                        neighbours[0] = logicTable[j-1, height-1];
+                        neighbours[1] = logicTable[j-1, k];
+                        neighbours[2] = logicTable[j-1, k + 1];
+
+                        neighbours[3] = logicTable[j, height - 1];
+                        neighbours[4] = logicTable[j, k + 1];
+
+                        neighbours[5] = logicTable[j + 1, height - 1];
+                        neighbours[6] = logicTable[j + 1, k];
+                        neighbours[7] = logicTable[j + 1, k + 1];
+                    }
+                    if (j + 1 == width && 0 < k && k < height - 1)
+                    {
+                        neighbours[0] = logicTable[j - 1, k-1];
+                        neighbours[1] = logicTable[j - 1, k];
+                        neighbours[2] = logicTable[j - 1, k + 1];
+
+                        neighbours[3] = logicTable[j, k - 1];
+                        neighbours[4] = logicTable[j, k + 1];
+
+                        neighbours[5] = logicTable[0 , k - 1];
+                        neighbours[6] = logicTable[0 , k];
+                        neighbours[7] = logicTable[0 , k + 1];
+                    }
+                    if (k + 1 == height && 0 < j && j < width - 1)
+                    {
+                        neighbours[0] = logicTable[j - 1, k - 1];
+                        neighbours[1] = logicTable[j - 1, k];
+                        neighbours[2] = logicTable[j - 1, 0];
+
+                        neighbours[3] = logicTable[j, k - 1];
+                        neighbours[4] = logicTable[j, 0];
+
+                        neighbours[5] = logicTable[j + 1, k - 1];
+                        neighbours[6] = logicTable[j + 1, k];
+                        neighbours[7] = logicTable[j + 1, 0];
+                    }
+                    if(j == 0 && k == 0)
+                    {
+                        neighbours[0] = logicTable[width-1, height-1];
+                        neighbours[1] = logicTable[width-1, 0];
+                        neighbours[2] = logicTable[width - 1, 1];
+
+                        neighbours[3] = logicTable[0, height-1];
+                        neighbours[4] = logicTable[0, 1];
+
+                        neighbours[5] = logicTable[1, height - 1];
+                        neighbours[6] = logicTable[1, 0];
+                        neighbours[7] = logicTable[1, 1];
+                    }
+                    if (j == width - 1 && k == 0)
+                    {
+                        neighbours[0] = logicTable[width - 2, height - 1];
+                        neighbours[1] = logicTable[width - 2, 0];
+                        neighbours[2] = logicTable[width - 2, 1];
+
+                        neighbours[3] = logicTable[width - 1, height - 1];
+                        neighbours[4] = logicTable[width - 1, 1];
+
+                        neighbours[5] = logicTable[0, height - 1];
+                        neighbours[6] = logicTable[0, 0];
+                        neighbours[7] = logicTable[0, 1];
+                    }
+                    if (j == width - 1  && k == height - 1)
+                    {
+                        neighbours[0] = logicTable[width - 2, height - 2];
+                        neighbours[1] = logicTable[width - 2, height - 1];
+                        neighbours[2] = logicTable[width - 2, 0];
+
+                        neighbours[3] = logicTable[width - 1, height - 2];
+                        neighbours[4] = logicTable[width - 1, 0];
+
+                        neighbours[5] = logicTable[0, height - 2];
+                        neighbours[6] = logicTable[0, height - 1];
+                        neighbours[7] = logicTable[0, 0];
+                    }
+                    if (j == 0 && k == height - 1)
+                    {
+                        neighbours[0] = logicTable[width - 1, height - 2];
+                        neighbours[1] = logicTable[width - 1, height - 1];
+                        neighbours[2] = logicTable[width - 1, 0];
+
+                        neighbours[3] = logicTable[0, height - 2];
+                        neighbours[4] = logicTable[0, 0];
+
+                        neighbours[5] = logicTable[1, height - 2];
+                        neighbours[6] = logicTable[1, height - 1];
+                        neighbours[7] = logicTable[1, 0];
+                    }
+                    if(j > 0 && k > 0 && j < width-1 && k < height-1)
+                    {
+                        neighbours[0] = logicTable[j - 1 , k-1];
+                        neighbours[1] = logicTable[j - 1, k];
+                        neighbours[2] = logicTable[j - 1, k+1];
+
+                        neighbours[3] = logicTable[j, k-1];
+                        neighbours[4] = logicTable[j, k+1];
+
+                        neighbours[5] = logicTable[j + 1, k - 1];
+                        neighbours[6] = logicTable[j + 1, k];
+                        neighbours[7] = logicTable[j + 1, k + 1];
+                    }
+
+                    if (logicTable[j, k] == false && neighbours.Where(c => c).Count()==3)
+                    {
+                        logicTable2[j, k] = true;
+                    }
+                    if (logicTable[j, k] == true && neighbours.Where(c => c).Count() > 3)
+                    {
+                        logicTable2[j, k] = false;
+                    }
+                    if (logicTable[j, k] == true && neighbours.Where(c => c).Count() < 2)
+                    {
+                        logicTable2[j, k] = false;
+                    }
+                    if (logicTable[j, k] == true && neighbours.Where(c => c).Count() == 2 || neighbours.Where(c => c).Count() == 3)
+                    {
+                        logicTable2[j, k] = true;
+                    }
+
+                }               
+            }
+            
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isWorking = false;
+            for (int i = 0; i < width; ++i)
+                for (int j = 0; j < height; j++)
+                {
+                    {
+                        logicTable[i, j] = false;
+                    }
+                }
+            iteration = 0;
+            licznik = 1;
+            pictureBox1.Refresh();
+            button1.Visible = true;
+
+        }
+    }
+}
